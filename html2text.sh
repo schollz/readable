f0742e732b42673c2c267f1deeb54a2d8932b25a
Zack <zack.scholl@gmail.com>
2017-07-22 06:48:53 -0600

    Update html2text.sh

--- html2text.sh
+++ html2text.sh
@@ -3,4 +3,4 @@
 # sudo apt-get install curl jq pandoc
 # https://mercury.postlight.com/web-parser/ for API key
 # ./html2text.sh APIKEY URL
-curl -s -H "x-api-key: $1" "https://mercury.postlight.com/parser?url=$2" | jq '.content' | pandoc -f html -t plain | sed 's/\\n//g' | sed 's/_//g' | sed 's/\[\]//g' | sed ':1;s/([^)]*)//g;/(/{N;b1};' | tail -n +3 | head -n -2
+curl -s -H "x-api-key: $1" "https://mercury.postlight.com/parser?url=$2" | jq '.content' | pandoc -f html -t plain | sed 's/\\n//g' | sed 's/_//g' | sed 's/\[\]//g' | sed ':1;s/([^)]*)//g;/(/{N;b1};' | tail -n +3 | head -n -2  | grep -v "^  "
