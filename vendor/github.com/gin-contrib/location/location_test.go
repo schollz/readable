ff7f2ec4a93e8109a6f41f5f837e0986c8c82139
Zack Scholl <zack.scholl@gmail.com>
2017-10-03 15:01:08 -0400

    Vendoring

---
+++ vendor/github.com/gin-contrib/location/location_test.go
@@ -0,0 +1,168 @@
+package location
+
+import (
+	"crypto/tls"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	gin.SetMode(gin.TestMode)
+}
+
+var tests = []struct {
+	want string
+	conf Config
+	req  *http.Request
+}{
+	// defaults
+	{
+		want: "http://localhost:8080",
+		conf: DefaultConfig(),
+		req: &http.Request{
+			Header: http.Header{},
+			URL:    &url.URL{},
+		},
+	},
+
+	// url scheme
+	{
+		want: "https://localhost:8080",
+		conf: DefaultConfig(),
+		req: &http.Request{
+			Header: http.Header{},
+			URL: &url.URL{
+				Scheme: "https",
+			},
+		},
+	},
+
+	// x-formward headers
+	{
+		want: "https://bar.com/bar",
+		conf: Config{"http", "foo.com", "/bar"},
+		req: &http.Request{
+			Header: http.Header{
+				"X-Forwarded-Proto": {"https"},
+				"X-Forwarded-For":   {"bar.com"},
+			},
+			URL: &url.URL{},
+		},
+	},
+
+	// X-Host headers
+	{
+		want: "http://bar.com/bar",
+		conf: Config{"http", "foo.com", "/bar"},
+		req: &http.Request{
+			Header: http.Header{
+				"X-Host": {"bar.com"},
+			},
+			URL: &url.URL{},
+		},
+	},
+
+	// URL Host
+	{
+		want: "http://bar.com/bar",
+		conf: Config{"http", "foo.com", "/bar"},
+		req: &http.Request{
+			Header: http.Header{},
+			URL: &url.URL{
+				Host: "bar.com",
+			},
+		},
+	},
+
+	// requests
+	{
+		want: "https://baz.com/bar",
+		conf: Config{"http", "foo.com", "/bar"},
+		req: &http.Request{
+			Proto:  "HTTPS://",
+			Host:   "baz.com",
+			Header: http.Header{},
+			URL:    &url.URL{},
+		},
+	},
+
+	// tls
+	{
+		want: "https://foo.com/bar",
+		conf: Config{"http", "foo.com", "/bar"},
+		req: &http.Request{
+			TLS:    &tls.ConnectionState{},
+			Header: http.Header{},
+			URL:    &url.URL{},
+		},
+	},
+}
+
+func TestLocation(t *testing.T) {
+	for _, test := range tests {
+		c := new(gin.Context)
+		c.Request = test.req
+		loc := newLocation(test.conf)
+		loc.applyToContext(c)
+
+		got := Get(c)
+
+		if got.String() != test.want {
+			t.Errorf("wanted location %s, got %s", got.String(), test.want)
+		}
+	}
+}
+
+func defaultRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(Default())
+
+	router.GET("/", func(c *gin.Context) {
+		url := Get(c)
+		c.String(200, url.String())
+	})
+
+	return router
+}
+
+func performRequest(r http.Handler, method string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestDefault(t *testing.T) {
+	router := defaultRouter()
+	w := performRequest(router, "GET")
+
+	assert.Equal(t, "http://localhost:8080", w.Body.String())
+}
+
+func customRouter(config Config) *gin.Engine {
+	router := gin.New()
+	router.Use(New(config))
+
+	router.GET("/", func(c *gin.Context) {
+		url := Get(c)
+		c.String(200, url.String())
+	})
+
+	return router
+}
+
+func TestCustom(t *testing.T) {
+	router := customRouter(Config{
+		Scheme: "https",
+		Host:   "foo.com",
+		Base:   "/base",
+	})
+	w := performRequest(router, "GET")
+
+	assert.Equal(t, "https://foo.com/base", w.Body.String())
+}
